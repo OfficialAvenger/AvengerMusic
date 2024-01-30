3095fe36a15ab7e888fcd149e9123cfe12571c42
OfficialAvenger <120937975+OfficialAvenger@users.noreply.github.com>
2024-01-30 17:46:25 +0530

    Avenger

--- AvengerMusic/utils/stream/stream.py
+++ AvengerMusic/utils/stream/stream.py
@@ -11,7 +11,7 @@ from AvengerMusic.misc import db
 from AvengerMusic.utils.database import add_active_video_chat, is_active_chat
 from AvengerMusic.utils.exceptions import AssistantErr
 from AvengerMusic.utils.inline import aq_markup, close_markup, stream_markup
-from AvengerMusic.utils.pastebin import InflexBin
+from AvengerMusic.utils.pastebin import AvengerBin
 from AvengerMusic.utils.stream.queue import put_queue, put_queue_index
 from AvengerMusic.utils.thumbnails import get_thumb
 
