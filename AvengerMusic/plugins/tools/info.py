0628fa66ee725258801daef302cbe550c36d2dbd
OfficialAvenger <120937975+OfficialAvenger@users.noreply.github.com>
2024-07-04 21:07:24 +0530

    Update info.py

--- AvengerMusic/plugins/tools/info.py
+++ AvengerMusic/plugins/tools/info.py
@@ -1,4 +1,4 @@
-from pyrogram import filters 
+from pyrogram import filters, enums, Client
 from pyrogram.types import Message
 from AvengerMusic import app
 
@@ -36,3 +36,31 @@ async def uid(client, message):
         text += f"The ID of Replied Chat/Channel: <code>{reply.sender_chat.id}</code>"
         
     await message.reply(text)
+
+
+
+@app.on_message(
+    filters.command("gifid") & (filters.group | filters.private),
+)
+async def get_gifid(_, m: Message):
+    if m.reply_to_message and m.reply_to_message.animation:
+        LOGGER.info(f"{m.from_user.id} used gifid cmd in {m.chat.id}")
+        await m.reply_text(
+            f"<b>Gif ID :</b>\n<code>{m.reply_to_message.animation.file_id}</code>",
+            parse_mode=enums.ParseMode.HTML,
+        )
+    else:
+        await m.reply_text(text="Please reply to a gif to get it's ID.")
+    return
+
+
+
+# Define the command handler
+@app.on_message(filters.command("stickerid"))
+async def stickerid(client, message):
+    # Check if the message is a reply and contains a sticker
+    if message.reply_to_message and message.reply_to_message.sticker:
+        sticker_id = message.reply_to_message.sticker.file_id
+        await message.reply_text(f"**The sticker ID is:**\n `{sticker_id}`")
+    else:
+        await message.reply_text("Please reply to a sticker to get its ID.")
