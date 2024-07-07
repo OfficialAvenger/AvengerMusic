4c255bdefea04ef40b39606d01fd7fe68c134714
OfficialAvenger <120937975+OfficialAvenger@users.noreply.github.com>
2024-07-07 08:41:02 +0530

    Update info.py

--- AvengerMusic/plugins/tools/info.py
+++ AvengerMusic/plugins/tools/info.py
@@ -1,6 +1,8 @@
+import os
 from pyrogram import filters, enums, Client
 from pyrogram.types import Message
-from AvengerMusic import app
+from pyrogram.enums import ParseMode
+from AvengerMusic import app, BANNED_USERS
 
 @app.on_message(filters.command("id"), group=10)
 async def uid(client, message):
@@ -66,3 +68,138 @@ async def stickerid(client, message):
         )
     else:
         await message.reply_text("Please reply to a sticker to get its ID.")
+
+
+
+
+
+
+n = "\n"
+w = " "
+
+bold = lambda x: f"**{x}:** "
+bold_ul = lambda x: f"**--{x}:**-- "
+mono = lambda x: f"`{x}`{n}"
+
+
+def section(title: str, body: dict, indent: int = 2, underline: bool = False) -> str:
+    text = (bold_ul(title) + n) if underline else bold(title) + n
+    for key, value in body.items():
+        text += (indent * w + bold(key) + ((value[0] + n) if isinstance(value, list) else mono(value)))
+    return text
+
+
+async def get_user_info(user, already=False):
+    if not already:
+        user = await app.get_users(user)
+    if not user.first_name:
+        return ["𝖣𝖾𝗅𝖾𝗍𝖾𝖽 𝖠𝖼𝖼𝗈𝗎𝗇𝗍", None]
+    user_id = user.id
+    username = user.username
+    first_name = user.first_name
+    mention = user.mention("ʟɪɴᴋ")
+    mlink = user.mention()
+    uname = ("@" + username) if username else mlink
+    dc_id = user.dc_id
+    photo_id = user.photo.big_file_id if user.photo else None
+    is_scam = user.is_scam
+    is_restricted = user.is_restricted
+    caption = f"""<b><u>𝖴𝖲𝖤𝖱 𝖨𝖭𝖥𝖮</u></b>
+
+<b>𝖭𝖺𝗆𝖾:</b> {first_name}
+<b>𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾:</b> {uname}
+<b>𝖬𝖾𝗇𝗍𝗂𝗈𝗇:</b> {mention}
+<b>𝖴𝗌𝖾𝗋 𝖨𝖣:</b> <code>{user_id}</code>
+<b>𝖣𝖢 𝖨𝖣:</b> <code>{dc_id}</code>
+<b>𝖲𝖼𝖺𝗆:</b> {is_scam}
+<b>𝖱𝖾𝗌𝗍𝗋𝗂𝖼𝗍𝖾𝖽:</b> {is_restricted}"""
+    return [caption, photo_id]
+
+
+async def get_chat_info(chat, already=False):
+    if not already:
+        chat = await app.get_chat(chat)
+    chat_id = chat.id
+    username = chat.username
+    title = chat.title
+    is_scam = chat.is_scam
+    description = chat.description
+    members = chat.members_count
+    is_restricted = chat.is_restricted
+    if username:
+        link = f"@{username}"
+    else:
+        try:
+            clink = await app.export_chat_invite_link(chat_id)
+            link = f"<a href={clink}>private</a>"
+        except:
+            link = "ᴘʀɪᴠᴀᴛᴇ"
+            pass
+    photo_id = chat.photo.big_file_id if chat.photo else None
+    caption = f"""<b><u>𝖢𝖧𝖠𝖳 𝖨𝖭𝖥𝖮</u></b>
+
+<b>𝖭𝖺𝗆𝖾:</b> {title}
+<b>𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾:</b> {link}
+<b>𝖢𝗁𝖺𝗍 𝖨𝖣:</b> <code>{chat_id}
+<b>𝖬𝖾𝗆𝖻𝖾𝗋𝗌:</b> <code>{members}</code>
+<b>𝖲𝖼𝖺𝗆:</b> {is_scam}
+<b>𝖱𝖾𝗌𝗍𝗋𝗂𝖼𝗍𝖾𝖽:</b> {is_restricted}
+<b><u>𝖣𝖾𝗌𝖼𝗋𝗂𝗉𝗍𝗂𝗈𝗇</u>:</b>
+<code>{description}</code>"""
+    return [caption, photo_id]
+
+
+@app.on_message(filters.command("info") & ~BANNED_USERS)
+async def info_func(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+    if message.reply_to_message:
+        user = message.reply_to_message.from_user.id
+    elif not message.reply_to_message and len(message.command) == 1:
+        user = message.from_user.id
+    elif not message.reply_to_message and len(message.command) != 1:
+        user = message.text.split(None, 1)[1]
+
+    m = await message.reply_text("𝖯𝗋𝗈𝖼𝖾𝗌𝗌𝗂𝗇𝗀......")
+    try:
+        info_caption, photo_id = await get_user_info(user)
+    except Exception as e:
+        return await m.edit(str(e))
+
+    if not photo_id:
+        return await m.edit(info_caption, disable_web_page_preview=True)
+    photo = await app.download_media(photo_id)
+
+    await message.reply_photo(photo=photo, caption=info_caption, quote=True, parse_mode=ParseMode.HTML)
+    await m.delete()
+    os.remove(photo)
+
+
+@app.on_message(filters.command("chatinfo") & ~BANNED_USERS)
+async def chat_info_func(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+    try:
+        if len(message.command) > 2:
+            return await message.reply_text("**ᴜsᴀɢᴇ:** /chatinfo [username|id]")
+        if len(message.command) == 1:
+            chat = message.chat.id
+        elif len(message.command) == 2:
+            chat = message.text.split(None, 1)[1]
+            
+        m = await message.reply_text("𝖯𝗋𝗈𝖼𝖾𝗌𝗌𝗂𝗇𝗀......")
+        info_caption, photo_id = await get_chat_info(chat)
+        if not photo_id:
+            return await m.edit(info_caption, disable_web_page_preview=True)
+
+        photo = await app.download_media(photo_id)
+        await message.reply_photo(photo=photo, caption=info_caption, quote=True, parse_mode=ParseMode.HTML)
+        await m.delete()
+        os.remove(photo)
+    except Exception as e:
+        await m.edit(e)
+     
