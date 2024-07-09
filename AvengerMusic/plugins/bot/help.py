8b339b69f5daeac70ce1bd24f3880daf7ce3e7f1
OfficialAvenger <120937975+OfficialAvenger@users.noreply.github.com>
2024-07-09 09:53:30 +0530

    Update help.py

--- AvengerMusic/plugins/bot/help.py
+++ AvengerMusic/plugins/bot/help.py
@@ -91,4 +91,4 @@ async def helper_cb(client, CallbackQuery, _):
     elif cb == "hb15":
         await CallbackQuery.edit_message_text(helpers.HELP_15, reply_markup=keyboard)
     elif cb == "hb17":
-        await CallbackQuery.edit_message_text(helpers.HELP_17, reply_markup=keyboard)
+        await CallbackQuery.edit_message_text(helpers.HELP_16, reply_markup=keyboard)
