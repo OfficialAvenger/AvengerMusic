44b1094c5d40c2031daa6d60debe2545c1a4a84a
OfficialAvenger <120937975+OfficialAvenger@users.noreply.github.com>
2024-07-21 07:59:48 +0530

    Update song.py

--- AvengerMusic/utils/inline/song.py
+++ AvengerMusic/utils/inline/song.py
@@ -1,19 +1,27 @@
 from pyrogram.types import InlineKeyboardButton
-
+import config
 
 def song_markup(_, vidid):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["P_B_1"],
+                text=_["SG_B_2"],
                 callback_data=f"song_helper audio|{vidid}",
             ),
             InlineKeyboardButton(
-                text=_["P_B_2"],
+                text=_["SG_B_3"],
                 callback_data=f"song_helper video|{vidid}",
             ),
         ],
+        [           
+            InlineKeyboardButton(
+                text="𝖸𝗈𝗎𝖳𝗎𝖻𝖾", url=f"https://youtube.com/"
+            ),
+        ],
         [
+            InlineKeyboardButton(
+                text="𝖲𝗎𝗉𝗉𝗈𝗋𝗍", url=f"{config.SUPPORT_GROUP}",
+            ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
             ),
